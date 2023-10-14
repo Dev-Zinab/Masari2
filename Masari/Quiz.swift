0da0e9c25f59b812cb1443e0423a879255326d49
zinab1991 <zbahakim23@twq.idserve.net>
2023-10-15 02:43:51 +0300

    somthing

---
+++ Masari/Quiz.swift
@@ -0,0 +1,18 @@
+//
+//  Quiz.swift
+//  Masari
+//
+//  Created by Zinab Zooba on 27/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Quiz: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Quiz()
+}
