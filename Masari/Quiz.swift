d0309b8716a5db7f4a6e62090ea05be1d87acd9a
zinab1991 <zbahakim23@twq.idserve.net>
2023-10-15 02:47:25 +0300

    1

--- Masari/Quiz.swift
+++ Masari/Quiz.swift
@@ -1,18 +1,98 @@
-//
-//  Quiz.swift
-//  Masari
-//
-//  Created by Zinab Zooba on 27/03/1445 AH.
-//
-
 import SwiftUI
 
-struct Quiz: View {
+
+struct ContentView: View {
+    @StateObject private var quiz = Quiz(questions: [
+        Question(text: "تحس بصعوبة لما تحاول تعرف بنفسك للناس؟", correctAnswer: true),
+        Question(text: "ما تحب تبادر وتبدأ بالكلام؟", correctAnswer: false),
+        Question(text: " ما يهمك تكون دايمًا محط انتباه الناس ؟", correctAnswer: false),
+        Question(text: "تاخذ وقت طويل عشان تبدأ في الفعاليات الاجتماعية في مكان جديد؟", correctAnswer: false),
+        Question(text: "تحس نفسك شخص هادي ومتحفظ نوعا ما؟", correctAnswer: false),
+        Question(text: " ما تحس إنك مبسوط ومستمتع لما تشوف مناسبات اجتماعية فيها انشطة ؟", correctAnswer: false),
+        Question(text: "تحس نفسك تعبان اكثر بعد ما قضيت وقت مع اصدقائك؟", correctAnswer: false),
+        Question(text: "غالبا تلقى صعوبات بأنك تتكلم قدام اشخاص كثار براحة؟", correctAnswer: false),
+        Question(text: "لو كنت في مكان زحمه، تتجنب انك تكون بالنص وتكون قريب من الجدار غالبا؟", correctAnswer: false),
+        Question(text: "ما تحب تبادر وتبدأ بالكلام؟", correctAnswer: false),
+        Question(text:"غالبا ما تكون نشيط ومتحمس؟", correctAnswer: false),
+        // Add more questions here
+    ])
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                if !quiz.isQuizComplete {
+                    QuestionView(question: quiz.currentQuestion, submitAnswer: submitAnswer)
+                } else {
+                    Text("Quiz Over! Your Score: \(quiz.calculateScore())")
+                }
+            }
+            .navigationTitle("الأسئلة")
+        }
     }
+    func submitAnswer(isAgree: Bool) {
+        quiz.submitAnswer(isAgree: isAgree)
+        quiz.moveToNextQuestion()
+    }
+    
 }
 
+struct QuestionView: View {
+    let question: Question
+    let submitAnswer: (Bool) -> Void
+
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            Text(question.text)// لاي شي؟
+                .font(.headline)
+                .padding()
+            HStack(spacing: 20) {
+                Button(action: {
+                    submitAnswer(true) // Agree
+                }) {
+                    Text("اوافــق")
+                        .padding()
+                        .background(Color(UIColor(red: 0.13, green: 0.18, blue: 0.33, alpha: 1.00)))
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                
+                Button(action: {
+                    submitAnswer(false) // Disagree
+                }) {
+                    Text("لا اوافق")
+                        .padding()
+                        .background(Color(UIColor(red: 0.13, green: 0.18, blue: 0.33, alpha: 1.00)))
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                    
+                }
+            }
+            
+            
+            // Spacer()
+            //   Text("\(Quiz.currentQuestion) / \(Quiz.questions.count)")
+            
+            //  ProgressView(value:Double(Quiz.currentQuestion)/Double(Quiz.questions.count))
+            //     .scaleEffect(CGSize(width: 0.8, height: 3.0))
+            //    .progressViewStyle(LinearProgressViewStyle(tint: Color(uiColor: UIColor(red: 0.96, green: 0.64, blue: 0.38, alpha: 1.00))))
+            
+            
+            
+            Text("\(Quiz.currentQuestion) / \(Quiz.questions.count)")
+            ProgressView(value: Int (Quiz.currentQuestionIndex)/Int (Quiz.questions.count))
+                .scaleEffect(CGSize(width: 1, height: 4))
+                .progressViewStyle(LinearProgressViewStyle())
+                .padding(.horizontal)
+        }
+
+    }
+    }
+
+
+
+
 #Preview {
-    Quiz()
+    ContentView()
 }
--- Masari/QuizClass.swift
+++ Masari/QuizClass.swift
@@ -1,18 +1,51 @@
-//
-//  QuizClass.swift
-//  Masari
-//
-//  Created by Zinab Zooba on 27/03/1445 AH.
-//
+import Foundation
 
-import SwiftUI
 
-struct QuizClass: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class Quiz: ObservableObject {
+    @Published var currentQuestion: Question
+     var questions: [Question]
+     var userAnswers: [Bool?]
+     var currentQuestionIndex: Int = 0
+
+    init(questions: [Question]) {
+        self.questions = questions
+        self.userAnswers = Array(repeating: nil, count: questions.count)
+        self.currentQuestion = questions[0]
+    }
+
+    var isQuizComplete: Bool {
+        userAnswers.allSatisfy { $0 != nil } //for each
+    }
+
+    func submitAnswer(isAgree: Bool) {              //مافهمت
+        userAnswers[currentQuestionIndex] = isAgree
+    }
+
+    func calculateScore() -> Int {
+        var score = 0
+
+        for (index, question) in questions.enumerated() {//قيم محددة مايخرج عنها بس هنا وين القيم مدخلتها 
+            if let userAnswer = userAnswers[index], userAnswer == question.correctAnswer {
+                score += 1
+            }
+        }
+
+        return score
+    }
+    var currentQuestionNumber: Int {
+            // Calculate the current question number based on the current index
+            return currentQuestionIndex + 1
+        }
+
+    func moveToNextQuestion() {
+        if currentQuestionIndex < questions.count - 1 {
+            currentQuestionIndex += 1
+            currentQuestion = questions[currentQuestionIndex]
+        }
     }
 }
 
-#Preview {
-    QuizClass()
-}
+struct Question {
+    let text: String
+    let correctAnswer: Bool // "Agree" (true) or "Disagree" (false)
+} 
