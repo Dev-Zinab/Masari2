ac9774d835ac6d08a88789bc6ecd49a99cfcdefb
zinab1991 <zbahakim23@twq.idserve.net>
2023-10-16 15:10:50 +0300

    Last modifide for Ms Lujin

---
+++ Masari/Quiz2.swift
@@ -0,0 +1,18 @@
+//
+//  Quiz2.swift
+//  Masari
+//
+//  Created by Zinab Zooba on 30/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Quiz2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Quiz2()
+}
